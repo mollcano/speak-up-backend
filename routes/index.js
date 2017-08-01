dfa37bfb4b5ff7c2f71d6e257da53d4c7857f98b
mollcano <MOLLYJBROOKS@GMAIL.COM>
2017-08-01 16:03:48 -0600

    watson console

--- routes/index.js
+++ routes/index.js
@@ -70,7 +70,7 @@ router.post('/addAudio/:id', (req, response) => {
   };
 
   speech_to_text.recognize(params, function(err, results) {
-    console.log(results.results, "results")
+    console.log(results.results[results.results.length-1], "results")
     if (err)
       console.log("hello");
     else
